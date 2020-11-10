2d1896889873b9fcd0aa93347caff9d839ef97dc
Mateskoda <skodamate93@gmail.com>
2020-11-10 23:41:25 +0100

    I had some conflicts with update, I had to use 'git stash push --include-untracked

--- src/Reader.java
+++ src/Reader.java
@@ -5,19 +5,30 @@ import java.util.Scanner;
 
 public class Reader {
     public static ArrayList<Person> persons = new ArrayList<>();
+    static Reader read = new Reader();
 
     public static void main(String[] args) throws FileNotFoundException {
-        Reader read = new Reader();
         read.read();
         read.persons.sort(new ComparatorForLastNamesInAlphabeticalOrder());
         for (Person chicago : Filter.cityFilter("Chicago", persons)) {
             System.out.println(chicago.getEmail());
         }
+        System.out.println(citizensOf("Chicago"));
+    }
 
+    //  give him one cityname, he will tell you the citizens
+    public static ArrayList<Person> citizensOf(String cityName) throws FileNotFoundException {
+
+        ArrayList<Person> citizensOf = Filter.cityFilter(cityName, read.persons);
+        if (citizensOf.isEmpty()) {
+            System.out.println("a városban lakó emberről nincs adatunk");
+        }
+
+        return citizensOf;
     }
 
     public static void read() throws FileNotFoundException {
-        Scanner sc = new Scanner(new File("us-500.csv"));
+        Scanner sc = new Scanner(new File("Chicago/us-500.csv"));
         sc.nextLine();
         for (int i = 0; sc.hasNext(); i++) {
             String line = sc.nextLine();
